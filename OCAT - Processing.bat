87ff46549e455aceff257886d350b33da0a61a34
GuestJim <30958420+GuestJim@users.noreply.github.com>
2018-07-10 18:38:41 -0400

    Update OCAT - Processing.bat

--- OCAT - Processing.bat
+++ OCAT - Processing.bat
@@ -1,8 +1,11 @@
 @echo off
+::	keeps the console window clean by suppressing the input commands
 
-pushd %~dp0"
+pushd %~dp0
+::	pushes the working directory
 
 :start
+::	a label named start
 
 python "OCAT - Processing.py" "%~1" "%~n1" "%~dp1
 ::	curiously I cannot close for the path
@@ -15,7 +18,10 @@ shift
 
 if "%~1"=="" goto end
 goto start
+::	checks if there are more files that were dragged-and-dropped onto the batch file to process and acts accordingly
 
 :end
+::	a label named end that will bring the script to its end
 
 ::pause
+::	pause is useful for troubleshooting so I tend to leave it commented out for future use
