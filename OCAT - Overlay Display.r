908951a46e588c1e1d20b14126cc12965d245ea0
GuestJim <30958420+GuestJim@users.noreply.github.com>
2019-01-06 20:07:57 -0500

    Update OCAT - Overlay Display.r

--- OCAT - Overlay Display.r
+++ OCAT - Overlay Display.r
@@ -1,34 +1,59 @@
-import sys, os, fileinput
-#	loads different modules for Python
+library(readr)
+#	loads the library for reading CSVs into R
+library(ggplot2)
+#	loads the GGPlot2 library for generating graphs
+library(foreach)
+library(doParallel)
+#	both foreach and doParallel are for making this multithreaded
 
-droppedFile = sys.argv[1]
-droppedName = sys.argv[2]
-droppedPath = sys.argv[3]
-#	assigns the values of the Batch Parameters passed to the Python script to these variables
+#setwd("!PATH!")
+#	sets the working directory
+#		checked and when not using the GUI, the scripts location is the working directory, so this is not necessary and impairs working across computers
+#		keeping it though for when working in the GUI though
+results <- read_csv("!FILEX!")
+#	reads the CSV to the results data frame
+dir.create("Frames - !FILE!", showWarnings=FALSE)
+#	creates a directory for the overlay graphs to be saved to
+setwd("Frames - !FILE!")
+#	sets the working directory to the folder for the graphs
 
-scriptPath = os.path.abspath('')
-#	gets the path of the Python script, which is the same path for the R source scripts
-scriptType = "OCAT"
-scriptName = "Overlay Display"
-#	separate Type and Name variables for use with the outputName variable below
-scriptFull = scriptPath + "\\" + scriptType + " - " + scriptName + ".r"
-#	full path and name of the source R script
-outputName = scriptName + " " + scriptType + " - " + droppedName + ".r"
-#	name of output R script
-outputFull = droppedPath + outputName
-#	full path and name of output R script
+count = 1/60
+back = 60
+#	how many previous frames to also show
 
-RPath = droppedPath.replace("\\", "/")
-#	R needs to use / instead of \ for file paths, hence this conversion
+pdf(NULL)
+#	to prevent an unnecessary PDF from being made
 
-os.chdir(droppedPath)
-#	changes the current working directory to where the CSV is
+registerDoParallel(cores=detectCores() - 4)
+#	sets how many threads can be used by detecting the number present and subtracting 4, for continued system usability
 
-from shutil import copyfile
-copyfile(scriptFull, outputFull)
-#	copies the source R script to the output R script location and name
+foreach (place=seq(from=0, to=round(max(results$TimeInSeconds)*60,0)-1, by=1), .packages = "ggplot2") %dopar% {
+#	the foreach command with %dopar% will spread the work across multiply threads
+#	foreach does require any necessary packages by identified, in this case ggplot2
+	
+theme_update(
+panel.background = element_rect(fill="black"),
+plot.background = element_rect(fill="black"), 
+axis.text = element_text(color="white", size=12),
+text = element_text(color="white", size=16),
+panel.grid.major=element_line(color="gray"),
+panel.grid.minor=element_line(color="gray20")
+)
+#	sets a custom theme for all of the graphs
+	
+ggplot(results, aes(TimeInSeconds,MsBetweenDisplayChange)) + 
+scale_y_continuous(name=NULL, breaks=round(seq(from=1, to=ceiling(max(results$MsBetweenDisplayChange*60/1000)), by=1)*1000/60,2),labels=seq(from=1, to=ceiling(max(results$MsBetweenDisplayChange*60/1000)), by=1), expand=c(0,0), limits=c(0,min(1001/6,max(results$MsBetweenDisplayChange))), sec.axis=dup_axis(), minor_breaks=NULL) + 
+scale_x_continuous(name=NULL,breaks=seq(from=0,to=round(max(results$TimeInSeconds), 0),by=1/6), labels=round(seq(from=0,to=round(max(results$TimeInSeconds), 0),by=1/6), 3), minor_breaks=seq(from=0,to=round(max(results$TimeInSeconds), 0),by=1/60), limits=c((place-back)*(count),(place)*(count)), expand=c(0,0)) + 
+expand_limits(y=c(0,1000/30)) + geom_step(color="green") + 
+geom_point(color="white", shape=9, size=2) #+ geom_path(color="white")
+#	makes the graphs
+	
+ggsave(filename=sprintf("%05d.png",place), device="png", width=20.48, height=3.84, dpi=100)
+#	I record at 2048x1152 so these images will be 2048x384 (one-third height)
 
-with fileinput.FileInput(outputName, inplace=True) as file:
-	for line in file:
-		print(line.replace("!PATH!", RPath).replace("!FILE!", droppedName).replace("!FILEX!", droppedName + ".csv"), end='')
-#	reads the lines of the outpur R script and replaces specific strings with the correct references
+dev.off(dev.prev())
+#	this closes the previous device, which is how the images are made and not closing them properly breaks R with too many devices
+}
+
+dev.off(dev.prev())
+#	also outside to make sure the devices are cleared
