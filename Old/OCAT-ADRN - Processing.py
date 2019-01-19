58db162b46302ce01303226724142d2229f681b8
GuestJim <30958420+GuestJim@users.noreply.github.com>
2019-01-19 15:49:54 -0500

    Add files via upload

---
+++ Old/OCAT-ADRN - Processing.py
@@ -0,0 +1,35 @@
+import sys, os, fileinput
+#	loads different modules for Python
+
+droppedFile = sys.argv[1]
+droppedADRN = sys.argv[2]
+droppedOCAT = sys.argv[3]
+droppedPath = sys.argv[4]
+#	assigns the values of the Batch Parameters passed to the Python script to these variables
+
+scriptPath = os.path.abspath('')
+#	gets the path of the Python script, which is the same path for the R source scripts
+scriptType = "OCAT-ADRN"
+scriptName = "Processing"
+#	separate Type and Name variables for use with the outputName variable below
+scriptFull = scriptPath + "\\" + scriptType + " - " + scriptName + ".r"
+#	full path and name of the source R script
+outputName = scriptName + " " + scriptType + " - " + droppedName + ".r"
+#	name of output R script
+outputFull = droppedPath + outputName
+#	full path and name of output R script
+
+RPath = droppedPath.replace("\\", "/")
+#	R needs to use / instead of \ for file paths, hence this conversion
+
+os.chdir(droppedPath)
+#	changes the current working directory to where the CSV is
+
+from shutil import copyfile
+copyfile(scriptFull, outputFull)
+#	copies the source R script to the output R script location and name
+
+with fileinput.FileInput(outputName, inplace=True) as file:
+	for line in file:
+		print(line.replace("!PATH!", RPath).replace("!FILEADRN!", droppedADRN).replace("!FILEOCAT!", droppedOCAT).replace("!FILEADRNX!", droppedADRN + ".csv").replace("!FILEOCATX!", droppedOCAT + ".csv"), end='')
+#	reads the lines of the outpur R script and replaces specific strings with the correct references
