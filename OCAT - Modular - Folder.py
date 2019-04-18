27bd2f7bb6f6c3ce98db74cc28510fa7270615fa
GuestJim <30958420+GuestJim@users.noreply.github.com>
2019-04-18 14:19:10 -0400

    Update OCAT - Modular - Folder.py
    Added code so it can read a Locations.txt location to replace the 'Recording #' text with the desired location.
    It will also read the quality from the file path, but if this is for a review, "Review" is used for the quality

--- OCAT - Modular - Folder.py
+++ OCAT - Modular - Folder.py
@@ -14,6 +14,7 @@ if " Performance Analysis" in droppedPath:
 	for i in range(len(droppedPath.split("\\"))):
 		if " Performance Analysis" in droppedPath.split("\\")[i]:
 			droppedGame = droppedPath.split("\\")[i].replace(" Performance Analysis", "") + " (" + droppedPath.split("\\")[i+2] + ")"
+			droppedQua = droppedPath.rsplit("\\", 2)[1]
 
 rec = 0
 for files in os.listdir(droppedPath):
@@ -39,5 +40,25 @@ for files in os.listdir(droppedPath):
 
 if not os.path.exists(droppedPath + "OCAT - Processing - Output.r"):
 	shutil.copyfile(scriptPath + "\\OCAT - Processing - Output.r", droppedPath + "OCAT - Processing - Output.r")
+	
+for file in os.listdir(droppedPath):
+	if "Locations.txt" in file:
+		loc = open(droppedPath + file, 'r').readlines()
+		loc = [line.rstrip('\n') for line in loc]
+
+try:	loc
+except:	sys.exit()
+
+for file in os.listdir(droppedPath):
+	if file.endswith(".r") and file.startswith("Processing"):
+		fileText = open(droppedPath + file,'r').read()
+
+		fout = open(droppedPath + file, 'w')
+
+		for i in range(len(loc)):
+			fileText = fileText.replace("Recording " + str(i + 1), droppedQua + " - " + loc[i])
+
+		fout.write(fileText)
+		fout.close()
 
 # os.system("pause")
