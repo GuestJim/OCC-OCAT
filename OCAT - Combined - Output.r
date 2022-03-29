92ab354a55935907015d9b2157b89e0f2998a5f3
GuestJim <mlcforeigner@gmail.com>
2022-03-29 13:10:59 -0400

    Scale Fill change
    By setting drop to FALSE in scale_fill_hue the colors for the bars in the Means graph will be consistent regardless of the number of GPUs in the data. This setting means colors are assigned to the factor levels, even if the factor is not present in the data.

--- OCAT - Combined - Output.r
+++ OCAT - Combined - Output.r
@@ -382,7 +382,7 @@ graphMEANS	=	function(datatype)	{
 	geom_hline(yintercept = 1000/60, color = "red") +
 	# geom_boxplot(outlier.alpha = 0) +
 	stat_summary(fun.data = BoxPerc, geom = "boxplot", width = 0.6) +
-	geom_bar(aes(fill = GPU), stat = "summary", fun = mean) + scale_fill_hue() +
+	geom_bar(aes(fill = GPU), stat = "summary", fun = mean) + scale_fill_hue(drop = FALSE) +
 	stat_summary(fun.data = BoxPerc, geom = "boxplot", alpha = 0.25, width = 0.6) +
 	# geom_boxplot(alpha = 0.50, outlier.alpha = 0.1) +
 	FACET(graphMEANS) +
